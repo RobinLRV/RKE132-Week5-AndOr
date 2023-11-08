636023a2ca720ca61f312eb88d62670ef44808b1
RobinLRV <robin.ellervee@gmail.com>
2023-11-08 14:38:54 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Random rnd = new Random();
+
+//int myRandomNum = rnd.Next(0, 11); //0-10
+//Console.WriteLine($"My random number is: {myRandomNum}");
+
+int myRandomNum;
+
+int randomsum = 0;
+
+
+
+for (int i = 0; i < 3; i++) 
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomsum = randomsum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+
+Console.WriteLine($"random sum total: {randomsum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//FOR-LOOP
+//DRY-principle - do not repeat yourself
+
+int i = 0;
+
+for (i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We dont need no education!");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("!");
+    
+//Console.WriteLine("We dont need no education!");
+//Console.WriteLine("We dont need no education!");
+//Console.WriteLine("We dont need no education!");
+//Console.WriteLine("We dont need no education!");
+//Console.WriteLine("We dont need no education!");
+
+
+
+
